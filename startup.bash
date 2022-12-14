fd761b5aecbf2addb6e7295746a393251b0a24fb
jared <jaredmales@gmail.com>
2022-12-14 01:44:08 +0000

    updated triggers; added some symlinks

--- startup.bash
+++ startup.bash
@@ -10,5 +10,8 @@ ln -s /milk/shm/camwfs_dark.im.shm /milk/shm/aol0_wfsdark.im.shm
 ln -s /milk/shm/camwfs_dark.im.shm /milk/shm/aol1_wfsdark.im.shm
 #ln -s /milk/shm/camwfs_ref.im.shm /milk/shm/aol0_wfsref.im.shm
 ln -s /milk/shm/aol1_wfsref.im.shm /milk/shm/camwfs_ref.im.shm 
+ln -s /milk/shm/aol1_wfsrefc.im.shm /milk/shm/camwfs_refc.im.shm
+ln -s /milk/shm/aol1_imWFS2.im.shm /milk/shm/camwfs_refsub.im.shm
+
 #/milk/shm/aol1_wfsref.im.shm
 
